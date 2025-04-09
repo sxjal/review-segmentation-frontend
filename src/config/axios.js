54532123b6b895dce4f57313ac387870d211cd61
sajal <sajalsahu8@gmail.com>
2025-04-10 04:26:50 +0530

    updated backend base url

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const axiosi = axios.create({
   withCredentials: true,
-  baseURL: "http://localhost:8000",
+  baseURL: "https://review-segmentation-backend.vercel.app",
 });
